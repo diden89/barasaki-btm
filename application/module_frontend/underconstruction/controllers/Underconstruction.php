211c844abad604a2b63f4a23aa4d9ff8fd59bc44
Dian Kosasih <algazasolution@gmail.com>
2021-07-14 10:31:51 +0700

    update all

---
+++ application/module_frontend/underconstruction/controllers/Underconstruction.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*!
+ * @package PT. Barasaki Sinergi Semesta
+ * @copyright PT. Barasaki Sinergi Semesta
+ * @author Algaza
+ * @version 1.0
+ * @access Public
+ * @path /D/xampp/htdocs/iwebebs/application/module_frontend/services/controllers/services.php
+ */
+
+class Underconstruction extends MY_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		
+	}
+
+	public function index()
+	{
+		// $this->store_params = array(
+		// 	"services" => $this->load_services($id),
+		// 	"slider" => $this->load_slider($id)
+		// );
+
+		$this->load->view('underconstruction_view');
+	}
+
+	public function load_services($id)
+	{
+		$services = $this->sm->load_services($id)->result();
+		return $services;
+	}
+	
+	public function load_slider($id)
+	{
+		$slider = $this->sm->load_slider('menu',$id)->row();
+		
+		return $slider;
+	}
+
+	public function service()
+	{
+		$url =  $this->uri->segment(1).'/'.$this->uri->segment(2).'/'.$this->uri->segment(3);
+		
+		$get_menu_id = $this->sm->get_menu($url)->row();
+		
+		$this->index($get_menu_id->id);
+	}
+
+}
